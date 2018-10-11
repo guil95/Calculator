9beeca689359730987d060525a7e946298668b02
Guilherme Rodrigues <guilhermerodriguestb@gmail.com>
2018-10-11 14:09:28 -0300

    Update OperationAbstract.php

--- app/Operations/OperationAbstract.php
+++ app/Operations/OperationAbstract.php
@@ -7,7 +7,7 @@ abstract class OperationAbstract implements OperationInterface{
     protected $number1;
     protected $number2;
 
-    public function __construct($number1, $number2 = null)
+    public function __construct(float $number1, float $number2 = null)
     {
         $this->number1 = $number1;
         $this->number2 = $number2;
